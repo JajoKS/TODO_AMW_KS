dad00fab234821ac4f0a01e348d64ddafb80638d
KRZYSZTOF SERAFIN <25102@KI.LOCAL>
2025-05-28 09:52:40 +0200

    testowanie, finish?

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'https://todo-amw-ks-backend.onrender.com' // wszystkie zapytania do /api trafią do Twojego backendu
+    }
+  }
 });
